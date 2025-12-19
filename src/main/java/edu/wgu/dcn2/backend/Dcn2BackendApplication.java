ca19de084dab9c3c744e55457cfa508bf2114a36
andyjimenez956 <andyjimenez956@gmail.com>
2025-12-18 20:39:55 -0600

    A - Spring Initializr Project Created With Required Dependencies

---
+++ src/main/java/edu/wgu/dcn2/backend/Dcn2BackendApplication.java
@@ -0,0 +1,13 @@
+package edu.wgu.dcn2.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcn2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dcn2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/wgu/dcn2/backend/Dcn2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wgu.dcn2.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dcn2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
