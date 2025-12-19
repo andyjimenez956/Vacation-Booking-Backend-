4029092c2569df55272aef61a5e0940d80ac4bba
andyjimenez956 <andyjimenez956@gmail.com>
2025-12-19 17:21:15 -0600

    Add spring data JPA repositories for REST API

---
+++ src/main/java/edu/wgu/dcn2/backend/dao/CustomerRepository.java
@@ -0,0 +1,11 @@
+package edu.wgu.dcn2.backend.dao;
+
+import edu.wgu.dcn2.backend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
